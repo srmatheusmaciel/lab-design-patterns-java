3988b8643a4dac690ce2a153077d5c74528c0ef2
Matheus Maciel <srmatheusmaciel@hotmail.com>
2024-10-18 11:46:47 -0300

    Adicionando projeto com Singleton e Strategy
    Signed-off-by: Matheus Maciel <srmatheusmaciel@hotmail.com>

---
+++ src/com/gof/singleton/SingletonLazy.java
@@ -0,0 +1,21 @@
+package com.gof.singleton;
+
+public class SingletonLazy {
+
+
+
+
+    private static SingletonLazy instance;
+
+    private SingletonLazy() {
+        super();
+    }
+
+    public static SingletonLazy getInstance() {
+        if (instance == null) {
+            instance = new SingletonLazy();
+        }
+        return instance;
+    }
+
+}
